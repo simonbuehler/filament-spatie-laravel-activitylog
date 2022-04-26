be64643554150c57847b1cc3355423b503aed59b
Simon Bühler <simon@00010111.de>
2022-04-26 14:37:26 +0200

    use not derived resources

--- src/ResourceFinder.php
+++ src/ResourceFinder.php
@@ -11,9 +11,12 @@ final class ResourceFinder
         $resources = Filament::getResources();
 
         foreach ($resources as $resource) {
-            $model = $resource::getModel();
+            $model             = $resource::getModel();
+            $trimmed_ressource = substr($resource, strrpos($resource, '\\') + 1);
+            $trimmed_class     = substr($class, strrpos($class, '\\') + 1);
 
-            if ($model === $class) {
+            if ($model === $class && str_starts_with($trimmed_ressource, $trimmed_class)
+            ) {
                 return $resource;
             }
         }
